fd0c25b679e669f7dcfa93fc3a9f1b01a89c6815
DANIELGEDAMO <danielgadamo@gmail.com>
2022-05-25 09:16:31 +0300

    l

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
-function dateAndTime(){
-    var myDate = new Date();
-    console.log(myDate);
-}
-// dateAndTime();
-function getStringPrintDate (date){
-    var myDte = new Date(date);
-    console.log(myDte);
-}getStringPrintDate('4/24/1995');
-function getString (){
-    var userDate =prompt('enter date');
-    var getDate = new Date(userDate);
-    return getDate.getMonth();
-} getString ();
-function getStringRiturnHours(){
-    var userTime =prompt('enter time');
-    var getTime = new Date(userTime);
-    return getTime.getHours();
-}getStringRiturnHours();
-function getRandomDate(){
-    var myDte = new Date();
-    var rndDay = Math.floor(math.rendom()*32);[]
-    var rndMonth = Math.floor(Math.random()* 12);
-    return myDte;
-}
-function getStringRiturnYear(){
-    var userDate = prompt('enter date');
-    var myYear = new Date(userDate);
-    return myYear.getFullYear();
-}getStringRiturnYear();
-function getStringRiturnMinets() {
-    var userstr = prompt('enter date');
-    var minets = new Date (userstr);
-    return minets.getMinutes();
+// function dateAndTime(){
+//     var myDate = new Date();
+//     console.log(myDate);
+// }
+// // dateAndTime();
+// function getStringPrintDate (date){
+//     var myDte = new Date(date);
+//     console.log(myDte);
+// }getStringPrintDate('4/24/1995');
+// function getString (){
+//     var userDate =prompt('enter date');
+//     var getDate = new Date(userDate);
+//     return getDate.getMonth();
+// } getString ();
+// function getStringRiturnHours(){
+//     var userTime =prompt('enter time');
+//     var getTime = new Date(userTime);
+//     return getTime.getHours();
+// }getStringRiturnHours();
+// function getRandomDate(){
+//     var myDte = new Date();
+//     var rndDay = Math.floor(math.rendom()*32);[]
+//     var rndMonth = Math.floor(Math.random()* 12);
+//     return myDte;
+// }
+// function getStringRiturnYear(){
+//     var userDate = prompt('enter date');
+//     var myYear = new Date(userDate);
+//     return myYear.getFullYear();
+// }getStringRiturnYear();
+// function getStringRiturnMinets() {
+//     var userstr = prompt('enter date');
+//     var minets = new Date (userstr);
+//     return minets.getMinutes();
 
-}getStringRiturnMinets();
-function getStringRiturnSooner(){
-    var date1 =prompt('enter date');
-    var date2 =prompt('enter date');
-    if (date1<date2){
-        return date1
-    }
-    else{
-        return date2
-    }
-}getStringRiturnSooner();
-function getStringRiturnAge(){
-    var userDate = prompt ('enter name');
-    var date = userDate
-}
+// }getStringRiturnMinets();
+// function getStringRiturnSooner(){
+//     var date1 =prompt('enter date');
+//     var date2 =prompt('enter date');
+//     if (date1<date2){
+//         return date1
+//     }
+//     else{
+//         return date2
+//     }
+// }getStringRiturnSooner();
+// function getStringRiturnAge(){
+//     var userDate = prompt ('enter date');
+//     var myDate = new Date().getFullYear()- new Date(userDate).getFullYear();
+//     return myDate;
+// }getStringRiturnAge()
+function getentre
